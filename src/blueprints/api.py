1d0537e6d59225df161612dd744ba8ca17857e1a
郝浪 <TobiasTao@qq.com>
2019-03-19 01:16:27 +0800

    前后端整合修正

--- src/blueprints/api.py
+++ src/blueprints/api.py
@@ -14,8 +14,10 @@ def translate():
     :return:
     """
     assert request.method == 'POST'
-    keyword = request.form.get('keyword', '')
-    lang_codes = request.form.get('lang_codes[]', []).split(',')
+    # keyword = request.data.form.get('keyword', '')
+    # lang_codes = request.data.form.get('lang_codes[]', []).split(',')
+    keyword = request.json.get('keyword', '')
+    lang_codes = request.json.get('lang_codes', '').split(',')
     try:
         r_result = rule_translator.translate(keyword, lang_codes)
     except Exception as e:
